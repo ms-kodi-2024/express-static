58934afc5044a0170e89f2e9ae4eec159a87220d
Marcin Stalmach <marcin.stalmach123@outlook.com>
2025-03-23 21:04:11 +0100

    Add Handlebars

--- server.js
+++ server.js
@@ -1,27 +1,39 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
-
-app.use((req, res, next) => {
-  res.show = name => {
-    res.sendFile(path.join(__dirname, 'views', name));
-  };
-  next();
-});
-
-app.use('/user', (req, res, next) => {
-  res.sendFile(path.join(__dirname, 'views', 'forbidden.html'));
-});
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/user', (req, res, next) => {
+  res.render('forbidden', {layout: false});
+});
+
 app.get(['/', '/home'], (req, res) => {
-  res.show('home.html'); 
+  res.render('home', {layout: false}); 
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: false });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { layout: false });
+});
+
+app.get('/info', (req, res) => {
+  res.render('info', { layout: false });
+});
+
+app.get('/history', (req, res) => {
+  res.render('history', { layout: false });
 });
 
 app.get('/image', (req, res) => {
