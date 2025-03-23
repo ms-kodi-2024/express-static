3740bbdeaf994950a9700a32914321370da3b8ea
Marcin Stalmach <marcin.stalmach123@outlook.com>
2025-03-23 21:45:16 +0100

    Add contact, history, info hbs && add dark theme for about

--- server.js
+++ server.js
@@ -3,45 +3,45 @@ const path = require('path');
 const hbs = require('express-handlebars');
 
 const app = express();
-app.engine('.hbs', hbs());
-app.set('view engine', '.hbs');
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './views/layouts', defaultLayout: 'main' }));
+app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/user', (req, res, next) => {
-  res.render('forbidden', {layout: false});
-});
-
 app.get(['/', '/home'], (req, res) => {
-  res.render('home', {layout: false}); 
+  res.render('home'); 
 });
 
 app.get('/hello/:name', (req, res) => {
-  res.render('hello', { layout: false, name: req.params.name });
+  res.render('hello', { name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', { layout: false });
+  res.render('about', { layout: 'dark' });
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact', { layout: false });
+  res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.render('info', { layout: false });
+  res.render('info');
 });
 
 app.get('/history', (req, res) => {
-  res.render('history', { layout: false });
+  res.render('history');
 });
 
 app.get('/image', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'images', 'image.png'));
 });
 
+app.use('/user', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'pages', 'forbidden.html'));
+});
+
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).sendFile(path.join(__dirname, 'views', 'pages', '404.html'));
 });
 
 app.listen(8000, () => {
